315d48267490504a616bb684cc455614d1f83fea
Sumnima B <sumnimabhandari@gmail.com>
2020-06-27 17:12:30 -0700

    Created burgers_db database and burgers table. Created schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
