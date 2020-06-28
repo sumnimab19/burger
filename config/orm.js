bab6b24de64063d0707be6401f6bfbe7ea5facc1
Sumnima B <sumnimabhandari@gmail.com>
2020-06-27 18:01:02 -0700

    Added code to orm.js

---
+++ config/orm.js
@@ -0,0 +1,56 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js");
+
+const orm = {
+    selectAll: function (tableInput, cb) {
+        let queryString = "SELECT * FROM ??";
+        connection.query(queryString, (err, result) => {
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+    },
+
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+}
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
